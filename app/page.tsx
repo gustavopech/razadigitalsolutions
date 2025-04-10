9a1db5a0de161b0d41e3c22ab9d9c4b64e14c2ba
Gustavo Pech <gustavopech2000@gmail.com>
2025-04-09 22:23:52 -0700

    next link edits

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,7 @@ import HeroSection from '@/components/HeroSection';
 import Location from '@/components/Location';
 import ServiceMenu from '@/components/ServiceMenu';
 import HomeStats from '@/components/HomeStats';
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
