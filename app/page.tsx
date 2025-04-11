a046dab42ec02a9d6266bde9ee35e8229e17ffbf
Gustavo Pech <gustavopech2000@gmail.com>
2025-04-11 02:22:45 -0700

    SEO tags and minor bug fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
-
+import Image from "next/image";
 import type { Metadata } from "next";
+import Navbar from '@/components/Navbar'
 import HeroSection from '@/components/HeroSection';
 import Location from '@/components/Location';
 import ServiceMenu from '@/components/ServiceMenu';
 import HomeStats from '@/components/HomeStats';
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ConsultationModal from "@/components/ConsulationModal";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
